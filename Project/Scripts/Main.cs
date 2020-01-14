5b9d7e963bb32d4feefe7c247e80a71ab4da353d
Mateus <mateusoliveiraspqr@gmail.com>
2020-01-14 20:55:32 -0300

    Added Physics: movement and gravity.

--- Project/Scripts/Main.cs
+++ Project/Scripts/Main.cs
@@ -2,8 +2,14 @@ using Godot;
 using System;
 
 public class Main : Node {
+
+    public static Vector2 UP                        = new Vector2(0, -1);
+    public static Node2D playerNode                 = new Node2D();
+    public static KinematicBody2D playerBodyNode    = new KinematicBody2D();
+    public static AnimatedSprite playerSpriteNode   = new AnimatedSprite();
+
     public override void _Ready() {
-        GD.Print("MainNode: Hello World!");
+
     }
 
 
--- Project/Scripts/Player.cs
+++ Project/Scripts/Player.cs
@@ -2,8 +2,89 @@ using Godot;
 using System;
 
 public class Player : Node2D {
+
+
+    public static Vector2 motion        = new Vector2();
+    public static int speed             = 70;
+    public static int runSpeed          = 100;
+    public static int jumpForce         = -760;
+    public static int gravityForce      = 180;
+    public static float slippery        = 0.2f;
+    public static string playerStatus   = "alive";
+    public static string keyPressed;
+
+
     public override void _Ready() {
-        GD.Print("PlayerNode: Hello Player!");
+        Main.playerNode = GetNode<Node2D>("/root/Main/Objects/Player");
+        Main.playerBodyNode = GetNode<KinematicBody2D>("/root/Main/Objects/Player/body");
+        Main.playerSpriteNode = GetNode<AnimatedSprite>("/root/Main/Objects/Player/body/sprite");
+
+    }
+
+
+    public override void _PhysicsProcess(float delta) {
+        // debug terminal commands.
+        GD.Print(motion);
+
+        // call basic methods.
+        GetInput();
+        Move();
+    }
+
+
+    public static void GetInput() {
+        // gets the input from the player and stores it in a variable.
+        if (Input.IsActionPressed("ui_right") && !Input.IsActionPressed("ui_left")) {
+            keyPressed = "right";
+        } else if (Input.IsActionPressed("ui_left") && !Input.IsActionPressed("ui_right")) {
+            keyPressed = "left";
+        } else if (!Input.IsActionPressed("ui_left") && !Input.IsActionPressed("ui_right")) {
+            keyPressed = "nil";
+        } else {
+            keyPressed = "both";
+        }
+
+    }
+
+
+    public static void Move() {
+        // moves the character using the physics engine, based on ----
+        // motion, i.e. velocity force applied in a single direction -
+        // and initialize gravity physics. ---------------------------
+        motion = Main.playerBodyNode.MoveAndSlide(motion, Main.UP);
+        motion.y = Mathf.Lerp(motion.y, gravityForce, slippery);
+
+
+        // switch the keys pressed and modify the motion to be applied.
+        switch (keyPressed) {
+            case ("right"):
+                GD.Print("MOVING RIGHT");
+                // flips the image.
+                Main.playerSpriteNode.FlipH = false;
+                // changes motion so it moves to the right.
+                if (!Main.playerBodyNode.IsOnFloor()) { 
+                    motion.x = Mathf.Lerp(motion.x, speed, slippery); 
+                    }
+                break;
+
+            case ("left"):
+                // flips the image.
+                Main.playerSpriteNode.FlipH = true;
+                // changes motion so it moves to the left.
+                if (!Main.playerBodyNode.IsOnFloor()) { 
+                    motion.x = Mathf.Lerp(motion.x, -speed, slippery); 
+                    }
+                break;
+            // case nor right or left key is pressed.
+            case ("nil"):   
+                // makes it stop, since it has no motion.
+                motion.x = Mathf.Lerp(motion.x, 0, slippery); 
+                break;
+
+            default:
+                break;
+        }
+
     }
 
 
