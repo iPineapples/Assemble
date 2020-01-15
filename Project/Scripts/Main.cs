71298a8acc58d0d98c11983fdabd3cde6f70f2d5
Mateus <mateusoliveiraspqr@gmail.com>
2020-01-15 16:20:46 -0300

    Better Movement, Jumping and Sliding.

--- Project/Scripts/Main.cs
+++ Project/Scripts/Main.cs
@@ -10,10 +10,15 @@ public class Main : Node {
     public static KinematicBody2D playerBodyNode    = new KinematicBody2D();
     public static AnimatedSprite playerSpriteNode   = new AnimatedSprite();
     public static Vector2 playerPos                 = new Vector2();
+    public static float tDelta;
 
 
     public override void _Ready() {
 
+    }
+
+    public override void _PhysicsProcess(float delta) {
+        tDelta = delta;
     }
 
 
--- Project/Scripts/Player.cs
+++ Project/Scripts/Player.cs
@@ -6,14 +6,17 @@ using System;
 public class Player : Node2D {
 
 
+    public static float tJump, tbJump   = 1.0f;
+
     public static Vector2 motion        = new Vector2();
     public static int speed             = 70;
     public static int runSpeed          = 100;
-    public static int jumpForce         = -760;
+    public static int jumpForce         = -600;
     public static int gravityForce      = 180;
-    public static float slippery        = 0.2f;
+    public static float slippery        = 0.1f;
     public static string playerStatus   = "alive";
     public static string keyPressed;
+    public static bool jumping          = false;
 
 
     public override void _Ready() {
@@ -31,7 +34,9 @@ public class Player : Node2D {
         
         // call basic methods.
         GetInput();
-        Move();
+        if (jumping == true) { Jump(); }
+        MoveAndGravity();
+        // Jump();
     }
 
 
@@ -47,13 +52,29 @@ public class Player : Node2D {
             keyPressed = "both";
         }
 
+        
+        if ((Input.IsActionJustPressed("ui_up") && Main.playerBodyNode.IsOnFloor() && jumping == false)) { 
+            jumping = true;
+            }
+        
+    /*
+        if (Input.IsActionJustPressed("ui_up")) {
+            if (Main.playerBodyNode.IsOnFloor()) {
+                jumping = true;
+            }
+        }
+    */
+        /*if (!Main.playerBodyNode.IsOnFloor()) {
+            jumping = false;
+        }*/
+
         // update Main Script with player's current position.
         Main.playerPos = Main.playerBodyNode.Position; 
 
     }
 
 
-    public static void Move() {
+    public static void MoveAndGravity() {
         // moves the character using the physics engine, based on ----
         // motion, i.e. velocity force applied in a single direction -
         // and initialize gravity physics. ---------------------------
@@ -89,4 +110,72 @@ public class Player : Node2D {
     }
 
 
+    public static void Jump() {
+
+        if (Main.playerBodyNode.IsOnFloor()) {
+            motion.y = jumpForce;
+        }
+
+        jumping = false;
+
+
+/*  THIS ONE EXECUTES ON 3 SECOND PERIODS
+        if (tJump >= 0.1f) {
+            tJump -= Main.tDelta;
+            if (motion.y <= -190) {
+                motion.y = Mathf.Lerp(motion.y, 200, 0.2f);
+            } else {
+                motion.y = Mathf.Lerp(motion.y, -200, 0.5f);
+            }
+            
+        } else {
+            tJump = 1.0f;
+            jumping = false;
+        }
+*/
+
+        // motion.y = Mathf.Lerp(motion.y, -600, 0.2f);
+
+
+/*        if (tJump >= 0.1f && jumping == true) {
+            GD.Print("JUMPING TIMER");
+            motion.y = Mathf.Lerp(motion.y, -300, 0.1f);
+            tJump -= Main.tDelta;
+            if (Main.playerBodyNode.IsOnFloor()) {
+                jumping = false;
+            }
+
+        } else {
+            GD.Print("ABC BITCHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+            tJump = tbJump;
+            jumping = false;
+        }
+*/
+/*        if (Input.IsActionJustPressed("ui_up") && Main.playerBodyNode.IsOnFloor()) {
+                motion.y = -200;
+                GD.Print("UP");
+                while (!Main.playerBodyNode.IsOnFloor()) {
+                    motion.y = Mathf.Lerp(motion.y, -300, 0.1f);
+                    GD.Print("AAA");
+                
+                }
+            }
+        }
+*/
+
+/* 
+            JUMPING
+            
+            when key up is pressed once:
+                if player is on the floor:
+                    StartJumping();
+            
+            StartJumping() {
+                while not on floor {
+                    lerp my motion.y to my jumpForce var;
+                }
+            }
+
+        */
+    }
 }
