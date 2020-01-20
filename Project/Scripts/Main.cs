4bdbd27fbe0abb7c93d8828d95bd8cbded487601
iPineapples <mateusoliveiraspqr@gmail.com>
2020-01-20 13:55:37 -0300

    Updated Godot to v3.2-RC2.

--- Project/Scripts/Main.cs
+++ Project/Scripts/Main.cs
@@ -5,26 +5,26 @@ using System;
 
 public class Main : Node {
 
-    // MOVEMENT
-    public static Vector2 UP                        = new Vector2(0, -1);
+	// MOVEMENT
+	public static Vector2 UP                        = new Vector2(0, -1);
 
-    // GLBOAL NODES
-    public static Node2D playerNode                 = new Node2D();
-    public static KinematicBody2D playerBodyNode    = new KinematicBody2D();
-    public static AnimatedSprite playerSpriteNode   = new AnimatedSprite();
+	// GLBOAL NODES
+	public static Node2D playerNode                 = new Node2D();
+	public static KinematicBody2D playerBodyNode    = new KinematicBody2D();
+	public static AnimatedSprite playerSpriteNode   = new AnimatedSprite();
 
-    // GLOBAL PROPERTIES/VARIABLES
-    public static Vector2 playerPos                 = new Vector2();
-    public static float tDelta;
-    public static int score;
+	// GLOBAL PROPERTIES/VARIABLES
+	public static Vector2 playerPos                 = new Vector2();
+	public static float tDelta;
+	public static int score;
 
 
-    public override void _PhysicsProcess(float delta) {
-        
-        // globally available delta time.
-        tDelta = delta;
-        
-    }
+	public override void _PhysicsProcess(float delta) {
+		
+		// globally available delta time.
+		tDelta = delta;
+		
+	}
 
 
 }
